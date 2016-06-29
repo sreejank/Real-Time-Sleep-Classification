a16b2858911baf822d033c5e89777cb3d9dd147b
Sreejan Kumar <sreejan.kumar@yale.edu>
2016-06-29 15:34:11 -0700

    June 29 work. Experimentation with new features

--- sleepclassify.py
+++ sleepclassify.py
@@ -9,12 +9,12 @@ from sklearn.cluster import KMeans
 from sklearn.cluster import DBSCAN
 from sklearn import tree
 import random
-"""
-Read test states from dat file. 
-0=EEG Channel 1
-1=EEG Channel 2
-2=EMG Channel 1
-"""
+
+#Read test states from dat file. 
+#0=EEG Channel 1
+#1=EEG Channel 2
+#2=EMG Channel 1
+
 def readChannels(fileName):
     print("Reading Channels from file...")
     a=[]
@@ -26,20 +26,20 @@ def readChannels(fileName):
     print("Done!")
     return arr
 
-"""
-Sequential search. 
-"""
+
+#Sequential search. 
+
 def find(arr,elem):
     for i in range(len(arr)):
         if arr[i]==elem:
             return i
 
-"""
-Read test states from dat file. 
-0=Wake
-1=NREM
-2=REM
-"""
+
+#Read test states from dat file. 
+#0=Wake
+#1=NREM
+#2=REM
+
 def readTestStates(fileName):
     print("Reading test states from file...")
     a=[]
@@ -55,9 +55,9 @@ def readTestStates(fileName):
     print("Done!")
     return ret
 
-"""
-Break up total channel array into epoch's 
-"""
+
+#Break up total channel array into epoch's 
+
 def epochs(channelArray,epochSize):
     i=0
     epochs=[]
@@ -67,9 +67,9 @@ def epochs(channelArray,epochSize):
         epochs.append(a)
     return epochs
 
-"""
-Bandpass filter lowcut to highcut. 
-"""
+
+#Bandpass filter lowcut to highcut. 
+
 def butter_bandpass(data,lowcut,highcut,fs,order=2):
     nyqValue=0.5*fs
     lowest=lowcut/nyqValue
@@ -77,28 +77,55 @@ def butter_bandpass(data,lowcut,highcut,fs,order=2):
     b,a=butter(order,[lowest,highest],btype='band')
     return lfilter(b,a,data)
 
-"""
-Compute power in [bin1,bin2] for an epoch
-"""
+
+#Compute power in [bin1,bin2] for an epoch
+
 def power(epochArray,bin1,bin2):
-    #powerscale=np.log10(np.abs(np.fft.rfft(epochArray)))
     bandpassed=butter_bandpass(epochArray,bin1,bin2,400)
     sumabs=sum(abs(bandpassed))
     if sumabs==0:
         return 0
     else:
         return math.log10(sumabs)
-    
-"""
-Analyze epoch's and return features. 
-"""
+
+#Compute "Autocorrelation metric" found in the seizure paper for a given EEG epoch. 
+
+def thetaAutoCorrelation(epochArray):
+    theta=butter_bandpass(epochArray,5,10,400)
+    i=0
+    minmaxes=[]
+    while(i+10<=len(theta)):
+        samp=theta[i:i+10]
+        extremes=(min(samp),max(samp))
+        minmaxes.append(extremes)
+        i+=10
+    i=0
+    sumdiff=0
+    while(i+2<len(minmaxes)):
+        si=minmaxes[i]
+        si1=minmaxes[i+1]
+        si2=minmaxes[i+2]
+        Hvi=min(si[1],max(si1[1],si2[1]))
+        Lvi=max(si[0],min(si1[0],si2[0]))
+        sumdiff+=Hvi-Lvi
+        i+=10
+
+    return sumdiff
+
+
+
+
+#Analyze epoch's and return features. 
+
 def analyzeEpochs(eegEpochs,emgEpochs):
     print("Calculating features...")
     delta=[]
     theta=[]
+    autocorr=[]
     for epoch in eegEpochs:
         delta.append(power(epoch,0.5,4))
         theta.append(power(epoch,5,10))
+        autocorr.append(thetaAutoCorrelation(epoch))
 
     emgPower=[]
     for epoch in emgEpochs:
@@ -117,23 +144,23 @@ def analyzeEpochs(eegEpochs,emgEpochs):
         ratios.append(ratio)
 
     print("Done!")
-    return (delta,theta,ratios,emgPower)
+    return (delta,theta,ratios,emgPower,autocorr)
 
-"""
-Normalizes features through zscore. 
-"""
-def normalize(delta,ratios,emgPower):
+
+#Normalizes features through zscore. 
+
+def normalize(*args):
     print("Normalizing features...")
-    normDelta=stats.zscore(delta)
-    normRatios=stats.zscore(ratios)
-    normEMGPower=stats.zscore(emgPower)
+    fin=[]
+    for arg in args:
+        fin.append(stats.zscore(arg))
     print("Done!")
-    return (normDelta,normRatios,normEMGPower)
+    return fin
 
-"""
-Classifies using kmeans algorithm (unsupervised). Eliminates all data >4 standard deviations in any of its features
-and assigns them random states.
-"""
+
+#Classifies using kmeans algorithm (unsupervised). Eliminates all data >4 standard deviations in any of its features
+#and assigns them random states.
+
 def kMeansCluster(delta,ratios,emgPower):
     print("Clustering...")
     data=[]
@@ -160,41 +187,32 @@ def kMeansCluster(delta,ratios,emgPower):
 
     print("Done!")
     return label
-"""
-Unsupervised classifying using thresholds defined by the best possible decision tree calculated. 
-"""
-def thresholdClassification(delta,ratios,emgPower):
-    calculated=[]
-    for i in range(len(delta)):
-        X=[delta[i],ratios[i],emgPower[i]]
 
-        if X[0]<=-0.018:
-            if X[2]<=.5:
-                if X[1]<=1.08:
-                    calculated.append(0)
-                else:
-                    calculated.append(2)
-            else:
+#Unsupervised classifying using thresholds defined by the best possible decision tree calculated. 
+
+def thresholdClassification(X):
+    calculated=[]
+    if X[0]<=-0.018:
+        if X[2]<=.5:
+            if X[1]<=1.08:
                 calculated.append(0)
+            else:
+                calculated.append(2)
         else:
-            if X[2]<=.6099:
-                calculated.append(1)
-            else:
-                calculated.append(0)
+            calculated.append(0)
+    else:
+        if X[2]<=.6099:
+            calculated.append(1)
+        else:
+            calculated.append(0)
 
     return calculated
 
 
 
-"""
-Create decision tree and output into graph visualization format (dot). Exports to graphFileName
-"""
-def decisionTree(delta,ratios,emgPower,labels,graphFileName):
-    data=[]
-    for i in range(len(delta)):
-        point=[delta[i],ratios[i],emgPower[i]]
-        data.append(point)
-    
+#Create decision tree and output into graph visualization format (dot). Exports to graphFileName
+
+def decisionTree(data,labels,graphFileName):
     data=np.asarray(data)
     model=tree.DecisionTreeClassifier(criterion='entropy',max_depth=3)
     model=model.fit(data,labels)
@@ -203,9 +221,9 @@ def decisionTree(delta,ratios,emgPower,labels,graphFileName):
     return (model.predict(data))
 
 
-"""
-Similarity between two state arrays. 
-"""
+
+#Similarity between two state arrays. 
+
 def printSimilarity(actual,ratings):
     actualCount=[0,0,0]
     calcCount=[0,0,0]
@@ -219,9 +237,9 @@ def printSimilarity(actual,ratings):
     print("REM. Calculated {} with actual {}".format(calcCount[2],actualCount[2]))
 
 
-"""
-Plot actual hypnogram and calculated hypnogram (from classification).
-"""
+
+#Plot actual hypnogram and calculated hypnogram (from classification).
+
 def plotHypnogram(actual,ratings):
     print("Calculating Hypnogram plot...")
     f,axarr=plt.subplots(2,sharex=True)
@@ -230,9 +248,9 @@ def plotHypnogram(actual,ratings):
     axarr[1].plot(ratings)
     axarr[1].set_title("Calculated")
     print("Done!")
-"""
-Plot REM Hypnogram (goes up to 1 if REM, 0 if not)
-"""
+
+#Plot REM Hypnogram (goes up to 1 if REM, 0 if not)
+
 def plotREMHypnogram(actual,ratings):
     actualREM=[]
     ratingREM=[]
@@ -248,20 +266,15 @@ def plotREMHypnogram(actual,ratings):
         else:
             actualREM.append(0)
 
-    plt.figure(1)
-    plt.plot(actualREM,label='Actual')
-    plt.plot(ratingREM,label='Calculated')
-
-    plt.figure(2)
     f,axarr=plt.subplots(2,sharex=True)
     axarr[0].plot(actualREM)
     axarr[0].set_title("Actual")
     axarr[1].plot(ratingREM)
     axarr[1].set_title("Calculated")
 
-"""
-Make 3 2D plots of the features. 
-"""
+
+#Make 3 2D plots of the features. 
+
 def plotThreeFeatures(delta,emgPower,ratios,ratings,actual):
     print("Calculating features plot...")
     colorsRatings=[]
@@ -315,9 +328,33 @@ def plotThreeFeatures(delta,emgPower,ratios,ratings,actual):
 
 
 
-"""
-Plot power spectrum transformed by real FFT. 
-"""
+#Plot how effective each feature is on bar graph. Does statistical tests that return a p value for each. 
+
+def evalFeatureEffectiveness(data,actual):
+    for index in range(len(data[0])):
+        featureArr=data[:,index]
+        group1,group2,group3=[],[],[]
+        for index2 in range(len(actual)):
+            if actual[index2]==0:
+                group1.append(featureArr[index2])
+            elif actual[index2]==1:
+                group2.append(featureArr[index2])
+            else:
+                group3.append(featureArr[index2])
+        
+        group1=np.asarray(group1)
+        group2=np.asarray(group2)
+        group3=np.asarray(group3)
+        anovaResults=stats.f_oneway(group1,group2,group3)
+        print("Feature {} has F={} and p={}".format(index,anovaResults[0],anovaResults[1]))
+
+
+
+
+
+
+#Plot power spectrum transformed by real FFT. 
+
 def plotPowerSpectrum(epoch):
     print("Calculating Power Spectrum...")
     f,axarr=plt.subplots(2)
@@ -330,15 +367,15 @@ def plotPowerSpectrum(epoch):
 
     print("Done!")
 
-"""
-Calculate size of epoch's given channels and number of state ratings. 
-"""
+
+#Calculate size of epoch's given channels and number of state ratings. 
 
 def calcEpochSize(channels,ratings):
     a=np.size(channels)
     b=len(ratings)
     return a/b
-##########################################
+
+################################################################################################################################################
 
 channels=readChannels('channels1040.dat')
 actual=readTestStates('teststates1040.dat')
@@ -348,17 +385,16 @@ size=calcEpochSize(channels[0],actual)
 eeg=epochs(channels[0],size)
 emg=epochs(channels[2],size)
 
-features=analyzeEpochs(eeg,emg) # (delta,theta,ratios,emgPower)
-normalized=normalize(features[0],features[2],features[3])
+features=analyzeEpochs(eeg,emg) # (delta,theta,ratios,emgPower,autocorr)
+normalized=normalize(features[0],features[2],features[3],features[4])
+data=[]
+for i in range(len(features[0])):
+    a=[normalized[0][i],normalized[1][i],normalized[2][i],normalized[3][i]]
+    data.append(a)
+data=np.asarray(data)
 
-#ratings=decisionTree(normalized[0],normalized[1],normalized[2],actual,'1032DecisionTree.dot')
-ratings=thresholdClassification(normalized[0],normalized[1],normalized[2])
+ratings=decisionTree(data,actual,'Entropy_Decision_Tree_Depth_3_With_Autocorrelation.dot')
 printSimilarity(actual,ratings)
-
-
-#plotHypnogram(actual,ratings)
-#plotPowerSpectrum(channels[2][0:size])
-#plotThreeFeatures(normalized[0],normalized[2],normalized[1],ratings,actual)
 plotREMHypnogram(actual,ratings)
 
 plt.show()
